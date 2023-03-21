5e1cda69bc22f2d7e29ce66ac1e58aab70072549
BAPradeep <102225268+BAPradeep@users.noreply.github.com>
2023-03-21 07:46:03 +0530

    Update last_digital.java
    removing for 100 to 10

--- P2_myprogram/src/Interview/last_digital.java
+++ P2_myprogram/src/Interview/last_digital.java
@@ -6,12 +6,12 @@ public class last_digital {
 		// TODO Auto-generated method stub
 		int number = 1234567895, firstdigit = 0,lastdigit=0;
 		
-		lastdigit = number%1000;
+		lastdigit = number%10;
 		System.out.println("Last digit: "+lastdigit);
 		
 		while(number!=0) {
-			firstdigit = number%100;
-			number /= 100;
+			firstdigit = number%10;
+			number /= 10;
 		}
 		System.out.println("First digit: "+firstdigit);
 		
