2b5baf348f83bcc1dc3d2d28fa5f1929fe604c67
BAPradeep <pradeep7manasa@gmail.com>
2023-05-06 13:52:46 +0530

    no message

---
+++ Java_Video_Full/src/SizeofDataTypes/size.java
@@ -0,0 +1,15 @@
+package SizeofDataTypes;
+
+public class size {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Size of int in Java: "+ (Integer.SIZE / 8) + " Byte");
+		System.out.println("Size of long in Java: "+ (Long.SIZE / 8) + " Byte");
+		System.out.println("Size of char in Java: "+ (Character.SIZE / 8) + " Byte");
+		System.out.println("Size of float in Java: "+ (Float.SIZE / 8) + " Byte");
+		System.out.println("Size of double in Java: "+ (Double.SIZE / 8) + " Byte");
+
+	}
+
+}
